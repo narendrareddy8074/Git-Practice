52701c377287eeeba1d496adc1f1a86f0c49e58c
narendrareddy04 <2311cs010659@mallareddyuniversity.ac.in>
2025-02-03 09:48:35 +0530

    greatest among 2 numbers

---
+++ Demo002.java
@@ -0,0 +1,14 @@
+public class Demo002 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int greatest;
+        if(a>b){
+            greatest = a;
+        }
+        else{
+            greatest = b;
+        }
+        System.out.println("The greatest number is: "+greatest);
+    }
+}
